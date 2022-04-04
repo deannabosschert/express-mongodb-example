6f8a1a8bea22aa2a245f5094b477e2b5a0706d76
deannabosschert <deanna.bosschert@gmail.com>
2022-04-05 00:04:34 +0200

    refactor: categorize modules

--- server.js
+++ server.js
@@ -1,40 +1,46 @@
-const express = require('express')
-const app = express()
-const session = require('express-session')
-const MongoDBSession = require('connect-mongodb-session')(session)
-const MongoClient = require('mongodb').MongoClient
-const bodyParser = require('body-parser') // to support JSON-encoded bodies
-const urlencodedParser = bodyParser.urlencoded({ extended: false }) // to support URL-encoded bodies
-let sessionID = 'sessionID'  
-let users_db = null // this will be the database object later on, needed a global variable to access it from other functions
+// Express/server necessities
+const express = require('express') 
+const app = express() // create an instance of express
+const session = require('express-session') // session middleware for handling sessions (cookies) for the express-server
+const bodyParser = require('body-parser') // to support JSON-encoded bodies, used in the post requests to the server (from HTML-forms)
+const urlencodedParser = bodyParser.urlencoded({ extended: false }) // to support URL-encoded bodies (from HTML-forms)
 
-const redirectUrl = require('./lib/redirect-url.js') // this is a custom module that I made to redirect the user to the correct page based on their session (logged in/not logged in)
-const editProfile = require('./lib/actions/edit.js')
-const loginProfile = require('./lib/actions/login.js')
-const registerProfile = require('./lib/actions/register.js')
-const removeProfile = require('./lib/actions/remove.js') 
+const port = process.env.PORT || 3000 // specify the port this server will listen to; if no port is specified, the server will listen to port 3000, but when deployed to Heroku, the port will be taken from the environment variable PORT (which Heroku sets)
 const upload = require('./lib/helpers/multer.js') // multer is a middleware that handles file uploads
-const liquidEngine = require('./lib/helpers/liquid.js') // this is the liquid engine
-
+const liquidEngine = require('./lib/helpers/liquid.js') // this is the liquid engine we'll use to render the HTML-templates with (liquid is a templating engine)
 require('dotenv').config() // this is a module that reads the '.env' file and sets the environment variables
 
-// MongoDB connection setup
+// My own modules/functions
+const redirectUrl = require('./lib/redirect-url.js') // this is a custom module that I made to redirect the user to the correct page based on their session (logged in/not logged in)
+const editProfile = require('./lib/actions/edit.js') // function to edit the user's profile
+const loginProfile = require('./lib/actions/login.js') // function to login the user
+const registerProfile = require('./lib/actions/register.js') // function to register the user to the database
+const removeProfile = require('./lib/actions/remove.js') // function to remove the user's profile from the database
+
+
+// Database (MongoDB) connection setup
+const MongoDBSession = require('connect-mongodb-session')(session)
+const MongoClient = require('mongodb').MongoClient
+
+let users_db = null // this will be the database object later on, needed a global variable to access it from other functions
 const url = process.env.DB_URL
-const port = process.env.PORT || 3000
+const dbName = process.env.DB_NAME
+const collection = process.env.C_NAME
 const options = { 
   useNewUrlParser: true,
   useUnifiedTopology: true
 }
+
 MongoClient.connect(url, options, (err, client) => { // connect to mongoDB
   if (err) { console.log('MongoDB-client connect error: ' + err) } // if there is an error, log it
-  else { users_db = client.db(process.env.DB_NAME).collection('users')}}) // if there is no error, set the users_db variable to the users collection
+  else { users_db = client.db(dbName).collection(collection)}}) // if there is no error, set the users_db variable to the users collection
 
 // Mongo Session setup
-const mongoSession = new MongoDBSession({ // create a new MongoDBSession object
+const MongoSession = new MongoDBSession({ // create a new MongoDBSession object
   uri: url,
-  collection: process.env.C_NAME
+  collection: collection
 })
-mongoSession.on('error', (err) => { console.log('MongoDB-session error:' + err) })  // error'afhandeling' mongodb session
+MongoSession.on('error', (err) => { console.log('MongoDB-session error:' + err) })  // if there is an error, log it
 
 
 app
@@ -44,9 +50,9 @@ app
   .set('views', './views') // specify the views directory
   .use(express.static('public')) // serve static files from the 'public' folder
   .use(session({ // set up the session
-    name: sessionID, // name of the cookie
+    name: 'sessionID', // name of the cookie
     secret: process.env.SESSION_SECRET, // secret for the cookie
-    store: mongoSession, // store the session in the database
+    store: MongoSession, // store the session in the database
     resave: false,  // don't save the session if it hasn't changed
     saveUninitialized: false, // don't save the session if it hasn't been modified
     cookie: { // set the cookie
@@ -58,7 +64,7 @@ app
   .get('/login', (req, res) => { redirectUrl(req, res, 'login', users_db) }) // zelfde als bij '/', maar geef dan de databaseinfo mee
   .post('/logout', (req, res) => { redirectUrl(req, res, 'logout', users_db) }) 
   .get('/profile', (req, res) => { redirectUrl(req, res, 'profile', users_db) })
-  .get('/remove', urlencodedParser, (req, res) => { res.render('pages/remove')})
+  .get('/remove', urlencodedParser, (req, res) => { res.render('pages/remove')}) // render the remove page
   .post('/login', urlencodedParser, (req, res) => { loginProfile(req, res, users_db) }) // wanneer het formulier dat als actie '/login' heeft, ontvang dan hier de gepostte data en voer dan de functie loginProfile uit
   .post('/signup', urlencodedParser, (req, res) => { registerProfile(req, res, users_db) })
   .post('/profile', upload.single('editImage'), urlencodedParser, (req, res) => { editProfile(req, res, users_db) }) // when the /edit form is submitted, upload the image and then edit the profile
